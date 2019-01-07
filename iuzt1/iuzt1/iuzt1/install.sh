f21c84d12ca9777ac496297d23acc2a687b289de
Andi B <41997217+andibde@users.noreply.github.com>
2019-01-07 15:09:50 +0100

    Update install.sh

--- iuzt1/iuzt1/iuzt1/install.sh
+++ iuzt1/iuzt1/iuzt1/install.sh
@@ -8,18 +8,23 @@
 #
 # Make sure you have `curl` installed
 echo Installing Ano Technology...
+sleep
 echo -ne '#####                     (33%)\r'
-sleep 1
+sleep 
 echo -ne '#############             (66%)\r'
-sleep 1
+sleep 
 echo -ne '#######################   (100%)\r'
 echo -ne '\n'
 echo Updating Version...
+sleep
 echo -ne '#####                     (33%)\r'
-sleep 1
+sleep 
 echo -ne '#############             (66%)\r'
-sleep 1
+sleep 
 echo -ne '#######################   (100%)\r'
 echo -ne '\n'
 echo Ano Technology successfully installed!
+sleep
+sleep
+sleep
 echo Now open Ano Browser and surf anonymous!
