d27ec196ba0bb5293eeccfc789dbfad88b17b1a1
Andi B <41997217+andibde@users.noreply.github.com>
2019-01-07 14:49:39 +0100

    Add files via upload

---
+++ iuzt1/iuzt1/iuzt1/install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# AnoVPN: Trivial OpenVPN 
+# http://andib.org/anovpn
+#
+# Install with this command (from your Linux machine):
+#
+# curl -L http://install.anovpn.andib.org | bash
+# Make sure you have `curl` installed
