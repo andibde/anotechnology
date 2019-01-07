291aa83fc6a2f2186260694641a36566a299a83a
Andi B <41997217+andibde@users.noreply.github.com>
2019-01-07 15:34:37 +0100

    Update install.sh

--- iuzt1/iuzt1/iuzt1/install.sh
+++ iuzt1/iuzt1/iuzt1/install.sh
@@ -7,6 +7,14 @@
 # curl -L http://install.anovpn.andib.org | bash
 #
 # Make sure you have `curl` installed
+echo yyyyyyhhdddhhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+echo yyyyhmNMMMMMNmhyyyyyy+../yyyyyyyyyyyyyyyyo:../oyyyyyyyyyyyy+.+yyyyyyyyyyyyyyys-.yyyyyyyyyyyyyyyyyyyy
+echo yyydMMmhyyddydMdyyyyy-..`ys/+//syo+++syyyys. yys+++oys+++oy+ :/+ys/+/+syo+++ss.`ys+++oys+/+/o+/yo/sy
+echo yyhNMmsshNNdhdMNhyyys`//`oo`-o -o..s`-syyys.`yy: o:`o: o/ o+ //`/o :/`:o`:o -s..y:`o-`o: o-`+: o--sy
+echo yyhNMdddyydNdNMNhyyy/`-:`:o.:y -s.-s.-yyyys.`yy: /::s: ss/so`++`+s /o`:o`/y :y-.y:.o:`o/ s:`os`:`/yy
+echo yyydNNddysshmMMdyyyy::ss:-o-+y-/y/-/-+yyyyy:-yyo-/:/so-/::ss-oo-os-+s-+s/:/-oy::yo-/::yo.:-`oy/`.syy
+echo yyyyhmNMNNMMNmhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyys+:-syy-/yyy
+echo yyyyyyhhhhhhhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyysyyyyyyyyyy
 echo Installing Ano Technology...
 echo -ne '#######################   (100%)\r'
 echo -ne '\n'
